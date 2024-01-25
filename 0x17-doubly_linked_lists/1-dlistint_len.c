dd9a4995e94d77a4daf8785d4360667863e0cf50
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-26 01:39:28 +0200

    Made Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of n
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	unsigned int n;
+
+	temp = h;
+	while (temp != 0)
+	{
+		n++;
+		temp = temp->next;
+	}
+	return (n);
+}
