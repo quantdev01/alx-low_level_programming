c8cb2d6225803e90889c6f310de5ce6d166b2ec4
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-26 20:09:00 +0200

    donewith 6th sum

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,21 +3,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - return the number of n
+ * sum_dlistint - return the number of n
  * @h: pointer to head of list
  * Return: the sum
  */
 
 int sum_dlistint(dlistint_t *h)
 {
-        const dlistint_t *temp;
-        int sum = 0;
+	const dlistint_t *temp;
+	int sum = 0;
 
-        temp = h;
-        while (temp != 0)
-        {
-                sum = sum + temp->n;
-                temp = temp->next;
-        }
-        return (sum);
+	temp = h;
+	while (temp != 0)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+	}
+	return (sum);
 }
