9d6ce0dad305519c2fe90e666f1a93ff969eb68e
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-26 17:41:21 +0200

    Made the question 02

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,27 +3,30 @@
 #include "lists.h"
 
 /**
- * add_nodeint - add node value at the beginning
+ * add_dnodeint - add node value at the beginning
  * @head: pointer head
  * @n: data to add
  * Return: node pointer
- * 
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode;
 	dlistint_t *temp = *head;
 
-	/* if (head == NULL)
-		return (0);
-	*/
-	newnode = (dlistint_t*)malloc(sizeof(dlistint_t));
+	newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+	{
+		printf("Error while creating the value\n");
+		return (NULL);
+	}
 
 	newnode->n = n;
 	newnode->prev = 0;
 	newnode->next = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = temp = newnode;
 	}
