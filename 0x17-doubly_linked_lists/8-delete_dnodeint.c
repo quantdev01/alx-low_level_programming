7ce129b7301d1a2128497bc8266442bfbb2e807e
Daniel Kiyiki <danielngolu154@gmail.com>
2024-01-27 02:37:39 +0200

    Maybe doe

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/*
+ * delete_dnodeint_at_index - node at index delete
+ * @head: head of the pointer
+ * @index: index of element to delete
+ * Return: return type
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *temp;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	temp = current->prev;
+	temp->next = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = temp;
+
+	free(current);
+
+	return (1);
+}
