6c3e5f96266d119e52473f2add07f9766b081b72
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-02-17 03:50:26 +0200

    Done

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - return index of the key 
+ *
+ * @key: key value
+ * @size: An array of size @size
+ * Return: return type
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+
+	return (hash % size);
+}
